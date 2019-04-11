f266fdae8b4e3f6cab2e64bf808e0e555082dba4
DrBeta <45474221+DrBeta@users.noreply.github.com>
2019-04-11 18:49:23 -0500

    Actually fix it this time

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,9 @@
 var helloLabel = document.querySelector('h1');
 var changeUserButton = document.querySelector('button');
-if(!localStorage.getItem('name')) {
-    setUserName();
-  } else {
-    var storedName = localStorage.getItem('name');
-    helloLabel.textContent = 'Hello, ' + storedName + '!';
-  }
+
 helloLabel.textContent = 'Hello!';
 function setUserName() {
     var myName = prompt('Please enter your name.');
-    localStorage.setItem('name', myName);
     
     if (myName === null || myName ===  ) {
      helloLabel.textContext = 'Hello!';   
