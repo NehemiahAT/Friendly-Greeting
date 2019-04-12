0c159b8ca6630e369194e81beaac72c205829109
Nehemiah <NehemiahAK354@outlook.com>
2019-04-11 20:30:07 -0500

    Revising main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,14 @@
-document.getElementById("btn1").onclick = function() {
-    setName();
+document.getElementById("btnOne").onclick = function() {
+setName();
 };
 
 function setName() {
-    var prompt = prompt("Please enter your name below:");
-    document.getElementById("helloH").textContext = "Hello, " + promt + "!";
-
+    var promt = prompt("Please enter your name below:", "John");
+    if (promt === null || promt === "") {
+document.getElementById("helloH").innerText = "Hello!";
+    }
+  else {
+document.getElementById("helloH").innerText = "Hello, " + promt + "!"; 
+  }
 }
 
