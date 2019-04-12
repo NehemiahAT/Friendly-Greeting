59ad7b833dac5f1727f18d1b023703568ccb1b32
Nehemiah <NehemiahAK354@outlook.com>
2019-04-12 14:30:18 -0500

    Setting Up Cookies

--- scripts/main.js
+++ scripts/main.js
@@ -9,5 +9,31 @@ function setName() {
     } else {
     document.getElementById("HelloH").innerText = "Hello, " + promt + "!";
     document.title = "Hello, " + promt + "!";
+    var createCookie = function(name, value, days) {
+    var expires;
+    if (days) {
+        var date = "Fri, 1 Jan 2100 12:00:00 UTC";
+        expires = "; expires=" + date;
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
+}
   }
 }
