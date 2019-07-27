01406761402339f6f7702dce77ba64f6b369d43e
Nehemiah <NehemiahAK354@outlook.com>
2019-07-27 14:24:34 -0500

    Add cookies and temporary troubleshooting
    I added some cookies to remember people's names as well as some temporary troubleshooting that is simple to understand but horribly complicated when coded in. I don't know that much about cookies and I am trying to see if this simple code will work.

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,45 @@
 document.getElementById("btnOne").onclick = function() {
 setName();
 };
-
+var promt;
+var checkOne;
+var ErrorAlertSwitchNumberOne;
 function setName() {
-    var promt = prompt("Please enter your name below:");
-    if (promt === null || promt === "") {
+   if (document.cookie === null || document.cookie === "") {
+       promt = prompt("Please enter your name below:");
+       }
+    else if (document.cookie !== null && document.cookie !== "") {
+     promt = document.cookie;   
+    }
+    else {
+     alert("Sorry, something went wrong. Check the console to troubleshoot.");
+     console.error("ErrorAlertSwitchOne");
+     ErrorAlertSwitchNumberOne = true;
+    }
+  if (promt === null || promt === "") {
+  checkOne = true; 
+  }
+  if (checkOne === true && ErrorAlertSwitchNumberOne === null) {
       document.getElementById("HelloH").innerText = "Hello!";
-    } else {
+    } else if (checkOne === true && ErrorAlertSwitchNumberOne === null) {
     document.getElementById("HelloH").innerText = "Hello, " + promt + "!";
     document.title = "Hello, " + promt + "!";
   }
+    else if (ErrorAlertSwitchNumberOne === true && checkOne === true) {
+     console.log("promt === null || promt === \"\"");   
+    }
+    else if (ErrorAlertSwitchNumberOne === true && promt !== null && promt !== "") {
+     console.log("promt === null && promt === \"\"");   
+    }
+    else {
+     alert("Sorry, something went wrong. Check the console to troubleshoot."); 
+     console.error("ErrorAlertSwitchTwo");
+     var ErrorAlertSwitchTwo = true;
+    }
 }
+
+function cookies() {
+ document.cookie = "name =" + promt;    
+}
+
+alert("This page uses cookies to remember your name the next time you visit it. If you do not give your name, no cookie will be created.")
